7553ad8465231120dab6a64c6908254325e61bc2
Fishcake <dj_fishcake@o2.pl>
2013-06-14 12:04:45 +0200

    tasks v0.00001

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,3 @@
 class List < ActiveRecord::Base
-  attr_accessible :title
+  attr_accessible :title, :description
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 ToDo::Application.routes.draw do
-  resources :lists
+  resources :lists do
+	  resources :tasks
+  end
+
+  root :to 				=> "lists#index"
+
 
 
   # The priority is based upon order of creation:
