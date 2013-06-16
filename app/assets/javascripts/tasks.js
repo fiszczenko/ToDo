b81fd4e81765946795aa170d4b449a394ba74bd9
Fishcake <dj_fishcake@o2.pl>
2013-06-16 20:34:41 +0200

    done na js

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function(){
+	$(".icon-remove").click(function(){
+		var $link = $(this).attr("href");
+		$(this).attr("class", "icon-ok");
+		$(this).attr("href", "#");
+		$.get($link)
+		return false;
+	});
+});
